947615409f3d27cb7e13d8c0c79759ffc6104691
David Melgarejo <davidarcosm@hotmail.com>
2023-01-12 22:23:06 -0600

    Portafolio design beging

---
+++ src/pages/Portafolio/Portafolio.jsx
@@ -0,0 +1,19 @@
+import "./css/style.css";
+
+export const Portafolio = () => {
+    return (
+        <div className="contenedorPrincipal">
+            <div className="previewFade">
+                <div className="previewFadeInfo">
+                    <h1 className="titulo">Never Gear</h1>
+                    <p className="descripcion">
+                        The NerveGear (ナーヴギア, Nāvugia?) is a second generation[1] of FullDive device made by Kayaba Akihiko, released in May 2022.
+                    </p>
+                </div>
+            </div>
+            <div className="detailsPreviewModel">
+
+            </div>
+        </div>
+    )
+}
